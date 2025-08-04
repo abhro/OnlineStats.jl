9822ce5a3551746258dc07f134fe1a63027d6d14
abhro <5664668+abhro@users.noreply.github.com>
2025-08-04 08:17:02 -0400

    Add syntax highlighting to more example code blocks

--- src/stats/linreg.jl
+++ src/stats/linreg.jl
@@ -67,7 +67,9 @@ Create an object from which any variable can be regressed on any other set of va
 optionally with element-wise ridge regularization.  The main function to use with
 `LinRegBuilder` is `coef`:
 
-    coef(o::LinRegBuilder, λ = 0; y=1, x=[2,3,...], bias=true, verbose=false)
+```julia
+coef(o::LinRegBuilder, λ = 0; y=1, x=[2,3,...], bias=true, verbose=false)
+```
 
 Return the coefficients of a regressing column `y` on columns `x` with ridge (`abs2` penalty)
 parameter `λ`.  An intercept (`bias`) term is added by default.
